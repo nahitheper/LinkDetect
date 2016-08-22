cdc701beaf9d5646ba16c8b952a7a76552cd289f
Nahit Rüştü HEPER <nahitheper@gmail.com>
2016-08-22 16:09:11 +0300

    Pod init

---
+++ Example/Pods/Target Support Files/LinkDetect/LinkDetect-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LinkDetect : NSObject
+@end
+@implementation PodsDummy_LinkDetect
+@end
---
+++ Example/Pods/Target Support Files/LinkDetect/LinkDetect-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double LinkDetectVersionNumber;
+FOUNDATION_EXPORT const unsigned char LinkDetectVersionString[];
+
